d6ddb32d86be4b596ff32014652d1f92198e7ec3
luojiahan <cuiyufei1234@126.com>
2023-09-18 14:08:10 +0800

    exec del directory

--- spark/src/main/scala/com/qingjiao/staff/WordCountTest.scala
+++ spark/src/main/scala/com/qingjiao/staff/WordCountTest.scala
@@ -1,7 +1,10 @@
 package com.qingjiao.staff
 
+import org.apache.commons.io.FileUtils
 import org.apache.spark.sql.SparkSession
 import org.apache.spark.{SparkConf, SparkContext}
+
+import java.io.File
 
 /**
  * spark-submit --master spark://hadoop000:7077 --class com.qingjiao.staff.WordCountTest /root/wordcount/sparkwordcount.jar
@@ -16,11 +19,11 @@ object WordCountTest {
       .getOrCreate()
     val sc: SparkContext = spark.sparkContext
 
-    val input = "file:///root/word.txt"
-    val output = "file:///root/wordcount/output1"
-    //val input = "file:///root/software/hadoop-2.7.7/README.txt"
-    //val output = "file:///root/wordcount/output2"
-    val lines = sc.textFile(input)
+    val input = "/root/word.txt"
+    val output = "/root/wordcount/output1"
+    //val input = "/root/software/hadoop-2.7.7/README.txt"
+    //val output = "/root/wordcount/output2"
+    val lines = sc.textFile("file://" + input)
     val words = lines.flatMap { line => line.split(" ") }
     val pairs = words.map { word => (word, 1) }
     val wordCounts = pairs.reduceByKey(_ + _)
@@ -31,7 +34,8 @@ object WordCountTest {
     //在将反转之后排序好的rdd在反转成：(hello ,3) (you , 2)
     val result = sortedCountWords.map(m => (m._2 , m._1))
 
-    result.saveAsTextFile(output)
+    FileUtils.deleteDirectory(new File(output))
+    result.saveAsTextFile("file://" + output)
 
     //result.map(x => "(" + x._1 + "," + x._2 + ")")
     //  .repartition(1)
